0f4e7048ecf3ee1e8f9105c1bbde794743326352
piotrek <pio_trek248@interia.pl>
2020-04-13 14:28:19 +0200

    Drugi commit

--- Kasa.cpp
+++ Kasa.cpp
@@ -1,13 +1,11 @@
 #include "Kasa.h"
 #include <iostream>
-#include <string>
-
 using namespace std;
 
 Kasa::Kasa(float b_200, float b_100, float b_50, float b_20, float b_10, float m_5, float m_2, float m_1, float m_05, float m_02, float m_01, float m_005,float m_002, float m_001)
 {
-    banknot_200=(float)b_200;
-    banknot_100=(float)b_100;
+    banknot_200=b_200;
+    banknot_100=b_100;
     banknot_50=b_50;
     banknot_20=b_20;
     banknot_10=b_10;
@@ -17,7 +15,7 @@ Kasa::Kasa(float b_200, float b_100, float b_50, float b_20, float b_10, float m
     moneta_05=m_05;
     moneta_02=m_02;
     moneta_01=m_01;
-    moneta_005=(float)m_005;
+    moneta_005=m_005;
     moneta_002=m_002;
     moneta_001=m_001;
 }
@@ -25,24 +23,24 @@ Kasa::Kasa(float b_200, float b_100, float b_50, float b_20, float b_10, float m
 void Kasa::otworz(){
     if(zamkniety){
         zamkniety = false;
-        cout << "Kasa została otwarta" << endl;
-    } else cout<<"Kasa jest już otwarta!";
+        cout << "Kasa została otwarta " << endl;
+    } else cout<<"Kasa jest już otwarta! " << endl;
 }
 
 void Kasa::zamknij(){
     if(!zamkniety){
         zamkniety = true;
-        cout << "Kasa została zamknięta";
-    } else cout<<"Kasa jest już zamknięta!";
+        cout << "Kasa została zamknięta " << endl;
+    } else cout<<"Kasa jest już zamknięta! " << endl;
 }
 
 void Kasa::dodaj(){
     float a;
     float ilosc;
     if(!zamkniety){
-        cout<<"Wpłać banknoty do kasy, wprowadź nominał w PLN (200, 100. 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01)" << endl;
+        cout<<"Wpłać banknoty do kasy, wprowadź nominał w PLN (200, 100. 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01) " << endl;
         cin >> a;
-        cout<<"Ile banknotów/monet wpłacasz?" << endl;
+        cout<<"Ile banknotów/monet wpłacasz? " << endl;
         cin >> ilosc;
         if(a == 20000) banknot_200 = banknot_200+ilosc;
         else if(a*100 == 10000) banknot_100 = banknot_100+ilosc;
@@ -58,15 +56,15 @@ void Kasa::dodaj(){
         else if(a*100 == 5) moneta_005 = moneta_005+ilosc;
         else if(a*100 == 2) moneta_002 = moneta_002+ilosc;
         else if(a*100 == 1) moneta_001 = moneta_001+ilosc;
-    } else cout << "Kasa jest zamknięta, przed wprowadzeniem zmian otworz kase.";
+    } else cout << "Kasa jest zamknięta, przed wprowadzeniem zmian otworz kase. " << endl;
 }
 
 void Kasa::odejmij(){
     float a;
     int ilosc;
     if(!zamkniety){
-        cout<<"Wypłać banknoty z kasy, wprowadź nominał w PLN (200, 100. 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01)" << endl; cin >> a;
-        cout<<"Ile banknotów/monet wypłacasz?" << endl; cin >> ilosc;
+        cout<<"Wypłać banknoty z kasy, wprowadź nominał w PLN (200, 100. 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01) " << endl; cin >> a;
+        cout<<"Ile banknotów/monet wypłacasz? " << endl; cin >> ilosc;
         if(a == 20000) banknot_200 = banknot_200-ilosc;
         else if(a*100 == 10000) banknot_100 = banknot_100-ilosc;
         else if(a*100 == 5000) banknot_50 = banknot_50-ilosc;
@@ -81,7 +79,7 @@ void Kasa::odejmij(){
         else if(a*100 == 5) moneta_005 = moneta_005-ilosc;
         else if(a*100 == 2) moneta_002 = moneta_002-ilosc;
         else if(a*100 == 1) moneta_001 = moneta_001-ilosc;
-    } else cout << "Kasa jest zamknięta, przed wprowadzeniem zmian otworz kase.";
+    } else cout << "Kasa jest zamknięta, przed wprowadzeniem zmian otworz kase. " << endl;
 }
 
 void Kasa::stan_kasy(){
